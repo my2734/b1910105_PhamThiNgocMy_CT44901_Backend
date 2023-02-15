fc06ee3dd3f2be264e5a9904c69bd43299abc529
my2734 <myb1910105@student.ctu.edu.vn>
2023-02-15 15:46:19 +0700

    Cai dat xu ly loi

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const contacts = require('../controller/contact.controller')
+const contacts = require('../controllers/contact.controller')
 
 const router = express.Router()
 
