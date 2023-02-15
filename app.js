18b6f5d4f0b9b0eac1d34d1b635d25d76073c8f2
my2734 <myb1910105@student.ctu.edu.vn>
2023-02-15 15:28:49 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.json({message: "Welcome to contact book application."})
+})
+
+module.exports = app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const cors = require('cors')
+// const contactsRouter = require('./app/routes/contact.route')
+// const ApiError = require('./app/api-error')
+
+// const app = express()
+
+// app.use(cors())
+// app.use(express.json())
+
+// app.use('/api/contacts',contactsRouter)
+
+// app.get('/', (req,res)=>{
+//     res.json({message: "Welcome to contact book application."})
+// })
+
+
+// //handle 404 response
+// app.use((req,res,next)=>{
+//     //Code o day se chay khi khong co route duoc dinh nghia nao
+//     //Khop voi yeu cau. Goi next() de chuyen san middleware xu li loi
+//     return next(new ApiError(404, "Resource not found"))
+// })  
+// //define error-handling middleware last, affter other app.use and routers calls
+// app.use((err,req,res,next)=>{
+//     //Middleware xu li loi tap trung
+//     //se chuyen ve middleware xu li loi nay
+
+//     // res.status(err.statusCode || 500).json({message: err.message || "Internal Server Error"})
+
+
+//     return res.status(err.statusCode || 500).json({
+//         message: err.message || "Internal Server Error"
+//     })
+// })
+
+// module.exports = app
